aefe662a62de0b29c764024bbd3f6e2b49e7ed73
Kayla Fitzsimmons <kayla@Kaylas-MBP.lan>
2023-09-19 17:26:48 -0700

    fix base config move to sveltekit & rename

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+            base: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
         }
 	}
 };
